faf5c5ddd83f5ea2c04c3b98a99d26e7fe0e8c17
AatroxGarcia <Aatroxlunag0w0@gmail.com>
2026-03-20 01:35:09 -0600

    api mails

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import authRoutes from './src/routes/auth.routes.js';
 import librosRoutes from './src/routes/libros.routes.js';
 import prestamosRoutes from './src/routes/prestamos.routes.js';
-
+import { iniciarScheduler } from './services/notificaciones.service.js';
 
 // Cargar variables de entorno
 dotenv.config();
@@ -21,7 +21,7 @@ const allowedOrigins = [
     process.env.FRONTEND_URL     
 ].filter(Boolean);
 
-
+iniciarScheduler();
 
 app.use(cors({
     origin: function (origin, callback) {
