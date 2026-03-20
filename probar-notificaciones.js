37a86adef0cdaadd75499fd29f842d1ede7b72fe
AatroxGarcia <Aatroxlunag0w0@gmail.com>
2026-03-20 01:48:22 -0600

    pruebas

--- probar-notificaciones.js
+++ probar-notificaciones.js
@@ -6,3 +6,5 @@ import { procesarNotificaciones } from './src/services/notificaciones.service.js
 console.log('Ejecutando prueba...');
 await procesarNotificaciones();
 process.exit(0);
+
+//node probar-procesarNotificaciones.js
