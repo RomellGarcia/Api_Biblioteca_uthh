686a1e4d97d770d8617495479c414282996adf17
RomellPC <Aatroxlunag0w0@gmail.com>
2026-03-25 00:35:52 -0600

    cambio

--- src/models/libros.model.js
+++ src/models/libros.model.js
@@ -29,7 +29,7 @@ async function obtenerCategorias() {
     const sql = `
         SELECT c.intidcategoria, c.vchcategoria, c.vchdescripcion, c.vchimagen
         FROM tblcategoria c
-        INNER JOIN tbllibros l ON c.intidcategoria = l.intidcategoria
+        LEFT JOIN tbllibros l ON c.intidcategoria = l.intidcategoria
         GROUP BY c.intidcategoria, c.vchcategoria, c.vchdescripcion, c.vchimagen
         ORDER BY c.vchcategoria ASC
     `;
