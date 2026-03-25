7028870f7353406f5a2e8de392706bc1173c1437
RomellPC <Aatroxlunag0w0@gmail.com>
2026-03-24 21:58:24 -0600

    prestamos

--- src/controllers/prestamos.controller.js
+++ src/controllers/prestamos.controller.js
@@ -202,20 +202,16 @@ async function postDevolucion(req, res) {
     }
 }
 
-
 const getMisPrestamos = async (req, res) => {
-    const { matricula } = req.params;
-
-    if (req.usuario.matricula !== matricula || req.usuario.idrol !== 3) {
-        return res.status(403).json({ mensaje: 'Acceso no autorizado' });
-    }
-
     try {
+        const { matricula } = req.params;
+        
         const rows = await obtenerPrestamosUsuario(matricula);
         res.json(rows);
+        
     } catch (error) {
         console.error('Error en getMisPrestamos:', error);
-        res.status(500).json({ mensaje: 'Error al obtener los préstamos' });
+        res.status(500).json({ mensaje: 'Error al obtener los préstamos', error: error.message });
     }
 };
 
